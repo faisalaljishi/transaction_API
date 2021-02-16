34a835767c815cee11c3a2073aaf127dadf519d5
Faisal <faisalaljishi@outlook.com>
2021-02-16 17:47:56 -0500

    First hosting/production changes to get ready for heroku set up

--- points/runcmds.bat
+++ points/runcmds.bat
@@ -1,6 +1,7 @@
 cd points
 python manage.py makemigrations
 python manage.py migrate
+export DJANGO_DEBUG=False
 ::python manage.py makemessages -l zh
 ::python manage.py compilemessages
 ::python manage.py test
