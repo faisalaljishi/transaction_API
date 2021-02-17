a71009efb68320d8dc244f286a20cca459d7b03a
Faisal <faisalaljishi@outlook.com>
2021-02-17 17:24:11 -0500

    Updated settings to try api requests from the cli

--- test_fill.cmd
+++ test_fill.cmd
@@ -1,16 +1,15 @@
-curl -i -X POST -H "Content-Type:application/json" -d "{\"user\": \"Joe Schmoe\", \"payer\": \"DANNON\", \"points\": 300}" http://django-points-api.herokuapp.com/api/create/
-curl -i -X POST -H "Content-Type:application/json" -d "{\"user\": \"Joe Schmoe\", \"payer\": \"UNILEVER\", \"points\": 200}" http://django-points-api.herokuapp.com/api/create/
-curl -i -X POST -H "Content-Type:application/json" -d "{\"user\": \"Joe Schmoe\",  \"points\": -200}" http://django-points-api.herokuapp.com/api/deduct/
-curl -i -X POST -H "Content-Type:application/json" -d "{\"user\": \"Joe Schmoe\", \"payer\": \"MILLER COORS\", \"points\": 10000}" http://django-points-api.herokuapp.com/api/create/
-curl -i -X POST -H "Content-Type:application/json" -d "{\"user\": \"Joe Schmoe\", \"payer\": \"DANNON\", \"points\": 1000}" http://django-points-api.herokuapp.com/api/create/
-curl -i -X POST -H "Content-Type:application/json" -d "{\"user\": \"Joe Schmoe\",  \"points\": -5000}" http://django-points-api.herokuapp.com/api/deduct/
+http --json POST http://django-points-api.herokuapp.com/api/create/ user="Joe Schmoe" payer="DANNON" points=300
+http --json POST http://django-points-api.herokuapp.com/api/create/ user="Joe Schmoe" payer="UNILEVER" points=200
+http --json POST http://django-points-api.herokuapp.com/api/deduct/ user="Joe Schmoe" points="-200"
+http --json POST http://django-points-api.herokuapp.com/api/create/ user="Joe Schmoe" payer="MILLER COORS" points=10000
+http --json POST http://django-points-api.herokuapp.com/api/create/ user="Joe Schmoe" payer="DANNON" points=1000
+http --json POST http://django-points-api.herokuapp.com/api/deduct/ user="Joe Schmoe" points="-5000"
 
-curl -i -X POST -H "Content-Type:application/json" -d "{\"user\": \"Jane Schmoe\", \"payer\": \"DANNON\", \"points\": 600}" http://django-points-api.herokuapp.com/api/create/
-curl -i -X POST -H "Content-Type:application/json" -d "{\"user\": \"Jane Schmoe\", \"payer\": \"DANNON\", \"points\": 600}" http://django-points-api.herokuapp.com/api/create/
-curl -i -X POST -H "Content-Type:application/json" -d "{\"user\": \"Jane Schmoe\",  \"points\": -1200}" http://django-points-api.herokuapp.com/api/deduct/
-curl -i -X POST -H "Content-Type:application/json" -d "{\"user\": \"Jane Schmoe\", \"payer\": \"MILLER COORS\", \"points\": 800}" http://django-points-api.herokuapp.com/api/create/
-curl -i -X POST -H "Content-Type:application/json" -d "{\"user\": \"Jane Schmoe\", \"payer\": \"DANNON\", \"points\": 500}" http://django-points-api.herokuapp.com/api/create/
-curl -i -X POST -H "Content-Type:application/json" -d "{\"user\": \"Jane Schmoe\", \"payer\": \"MILLER COORS\", \"points\": 400}" http://django-points-api.herokuapp.com/api/create/
-curl -i -X POST -H "Content-Type:application/json" -d "{\"user\": \"Jane Schmoe\", \"payer\": \"UNILEVER\", \"points\": 800}" http://django-points-api.herokuapp.com/api/create/
-curl -i -X POST -H "Content-Type:application/json" -d "{\"user\": \"Jane Schmoe\",  \"points\": -2500}" http://django-points-api.herokuapp.com/api/deduct/
-
+http --json POST http://django-points-api.herokuapp.com/api/create/ user="Jane Schmoe" payer="DANNON" points=600
+http --json POST http://django-points-api.herokuapp.com/api/create/ user="Jane Schmoe" payer="DANNON" points=600
+http --json POST http://django-points-api.herokuapp.com/api/deduct/ user="Jane Schmoe" points="-1200"
+http --json POST http://django-points-api.herokuapp.com/api/create/ user="Jane Schmoe" payer="MILLER COORS" points=800
+http --json POST http://django-points-api.herokuapp.com/api/create/ user="Jane Schmoe" payer="DANNON" points=500
+http --json POST http://django-points-api.herokuapp.com/api/create/ user="Jane Schmoe" payer="MILLER COORS" points=400
+http --json POST http://django-points-api.herokuapp.com/api/create/ user="Jane Schmoe" payer="UNILEVER" points=800
+http --json POST http://django-points-api.herokuapp.com/api/deduct/ user="Jane Schmoe" points="-2500"
--- test_fill.sh
+++ test_fill.sh
@@ -1,82 +1,15 @@
-https --form POST http://django-points-api.herokuapp.com/api/create/ code="print(123)"
-{
-        "user": "Joe Schmoe",
-        "payer": "DANNON",
-        "points": 300
-}
-https --form POST http://django-points-api.herokuapp.com/api/create/ code="print(123)"
-{
-        "user": "Joe Schmoe",
-        "payer": "UNILEVER",
-        "points": 200
-}
-https --form POST http://django-points-api.herokuapp.com/api/deduct/ code="print(123)"
-{
-        "user": "Joe Schmoe",
-        "points": -200
-}
-https --form POST http://django-points-api.herokuapp.com/api/create/ code="print(123)"
-{
-        "user": "Joe Schmoe",
-        "payer": "MILLER COORS",
-        "points": 10000
-}
-https --form POST http://django-points-api.herokuapp.com/api/create/ code="print(123)"
-{
-        "user": "Joe Schmoe",
-        "payer": "DANNON",
-        "points": 1000
+http --json POST http://django-points-api.herokuapp.com/api/create/ user="Joe Schmoe" payer="DANNON" points=300
+http --json POST http://django-points-api.herokuapp.com/api/create/ user="Joe Schmoe" payer="UNILEVER" points=200
+http --json POST http://django-points-api.herokuapp.com/api/deduct/ user="Joe Schmoe" points="-200"
+http --json POST http://django-points-api.herokuapp.com/api/create/ user="Joe Schmoe" payer="MILLER COORS" points=10000
+http --json POST http://django-points-api.herokuapp.com/api/create/ user="Joe Schmoe" payer="DANNON" points=1000
+http --json POST http://django-points-api.herokuapp.com/api/deduct/ user="Joe Schmoe" points="-5000"
 
-}
-https --form POST http://django-points-api.herokuapp.com/api/deduct/ code="print(123)"
-{
-        "user": "Joe Schmoe",
-        "points": -5000
-}
-,
-https --form POST http://django-points-api.herokuapp.com/api/create/ code="print(123)"
-{
-        "user": "Jane Schmoe",
-        "payer": "DANNON",
-        "points": 600
-}
-https --form POST http://django-points-api.herokuapp.com/api/create/ code="print(123)"
-{
-        "user": "Jane Schmoe",
-        "payer": "DANNON",
-        "points": 600
-}
-https --form POST http://django-points-api.herokuapp.com/api/deduct/ code="print(123)"
-{
-        "user": "Jane Schmoe",
-        "points": -1200
-}
-https --form POST http://django-points-api.herokuapp.com/api/create/ code="print(123)"
-{
-        "user": "Jane Schmoe",
-        "payer": "MILLER COORS",
-        "points": 800
-}
-https --form POST http://django-points-api.herokuapp.com/api/create/ code="print(123)"
-{
-        "user": "Jane Schmoe",
-        "payer": "DANNON",
-        "points": 500
-}
-https --form POST http://django-points-api.herokuapp.com/api/create/ code="print(123)"
-{
-        "user": "Jane Schmoe",
-        "payer": "MILLER COORS",
-        "points": 400
-}
-https --form POST http://django-points-api.herokuapp.com/api/create/ code="print(123)"
-{
-        "user": "Jane Schmoe",
-        "payer": "UNILEVER",
-        "points": 800
-}
-https --form POST http://django-points-api.herokuapp.com/api/deduct/ code="print(123)"
-{
-        "user": "Jane Schmoe",
-        "points": -2500
-}
+http --json POST http://django-points-api.herokuapp.com/api/create/ user="Jane Schmoe" payer="DANNON" points=600
+http --json POST http://django-points-api.herokuapp.com/api/create/ user="Jane Schmoe" payer="DANNON" points=600
+http --json POST http://django-points-api.herokuapp.com/api/deduct/ user="Jane Schmoe" points="-1200"
+http --json POST http://django-points-api.herokuapp.com/api/create/ user="Jane Schmoe" payer="MILLER COORS" points=800
+http --json POST http://django-points-api.herokuapp.com/api/create/ user="Jane Schmoe" payer="DANNON" points=500
+http --json POST http://django-points-api.herokuapp.com/api/create/ user="Jane Schmoe" payer="MILLER COORS" points=400
+http --json POST http://django-points-api.herokuapp.com/api/create/ user="Jane Schmoe" payer="UNILEVER" points=800
+http --json POST http://django-points-api.herokuapp.com/api/deduct/ user="Jane Schmoe" points="-2500"
